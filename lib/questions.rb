bca9c7ae5ecbff3410eed9c886c2dc1d6bc0fcf9
georn <mrog0@outlook.com>
2018-01-31 11:30:26 +0000

    Tdd first 10 exercises

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,22 +1,27 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+  array.select { |element| element =~ /^a/ } # => ["apples", "avocados"]
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  array.select { |element| element =~ /^[aeiou]/ } # => ['omar', 'idris', 'angela']
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  array.delete_if { |element| element.nil? } # => ['a', 'b', false, 'c']
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.delete_if { |element| element.nil? || element == false } # => ['a', 'b', 'c']
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+  array.each { |element| element.reverse! } # => ['god', 'yeknom', 'tnahpele']
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
@@ -24,20 +29,25 @@ end
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
 # make sure you don't have the same pairing twice,
 def every_possible_pairing_of_students(array)
+  array.combination(2).to_a # => [["Bob", "Dave"], ["Bob", "Clive"], ["Dave", "Clive"]]
 end
 
 # discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+  3.times { array.shift } # => 3
+  array # => [4, 5, 6]
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element) # => [element, ...]
 end
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+  array.sort_by { |word| word[-1] } # => ['puma', 'maker', 'sky']
 end
 
 # cut strings in half, and return the first half, e.g.
